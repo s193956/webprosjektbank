b6c2d00fab8da9aab8a43afacce38f3ce3ba901e
Arlen <Arlen@BRUTUS>
2016-11-11 13:22:40 +0100

    databsen funker nå

--- WebprosjektBankOblig/Models/LoggEntry.cs
+++ WebprosjektBankOblig/Models/LoggEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace WebprosjektBankOblig.Models
 {
     public class LoggEntry
     {
+        [Key]
         public int Id { get; set; }
         public string bruker { get; set; }
         public bool success { get; set; }
