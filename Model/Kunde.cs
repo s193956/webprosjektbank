ff926961d28114a5620eed24401a8779c5de14ba
Mohamed El Morabeti <mohamed_emnm@hotmail.com>
2016-11-13 15:08:45 +0100

    view

--- Model/Kunde.cs
+++ Model/Kunde.cs
@@ -11,9 +11,19 @@ namespace WebprosjektBankOblig.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Personnummer")]
+        [Required(ErrorMessage = "Personnummer må oppgis")]
+        [RegularExpression(@"[0-9]{11}", ErrorMessage = "Postnr må være 11 siffer")]
         public string Personnummer { get; set; }
+        [Display(Name = "Navn")]
+        [Required(ErrorMessage = "Navn må oppgis")]
         public string Navn { get; set; }
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "Adresse må oppgis")]
         public string Adresse { get; set; }
+        [Display(Name = "Mobil")]
+        [Required(ErrorMessage = "Mobil må oppgis")]
+        [RegularExpression(@"[0-9]{8}", ErrorMessage = "Mobil må være 8 siffer")]
         public string Tlf { get; set; }
 
         public bool slettet { get; set; }
